2031876bbe35cc1e4a35ee1c3d3f14edaa696768
Cactos <50751649+Markz1819@users.noreply.github.com>
2023-06-18 14:37:27 -0700

    Comments

--- ACSL_Assembly.cpp
+++ ACSL_Assembly.cpp
@@ -45,44 +45,43 @@ int main() {
         index++;
     }
     
-    int pointer = 0;
+    int pointer = 0; // points at which line of code to run
     while(true){
-        string LABEL,OPCODE,LOC;
-        LABEL = code[0][pointer];
-        OPCODE = code[1][pointer];
-        LOC = code[2][pointer];
-        if (!OPCODE.compare("LOAD")){
-            if (LOC[0] == '='){
+        string LABEL,OPCODE,LOC; //LABEL field, OPCODE field, LOC field
+        LABEL = code[0][pointer]; //sets LABEL as first
+        OPCODE = code[1][pointer]; //sets OPCODE as second
+        LOC = code[2][pointer]; //sets LOC as third
+        if (!OPCODE.compare("LOAD")){ 
+            if (LOC[0] == '='){ // if the LOC contains a value, put the value into ACC
                 ACC = stoi(LOC.substr(1));
-            }
-            else {
+            } else { // if the LOC contains a variable, put the variable into ACC
                 ACC = st[LOC];
             }
-        } else if (!OPCODE.compare("STORE")){
+        } else if (!OPCODE.compare("STORE")){ // put the variable into ACC
             st[LOC] = ACC;
 
-        } else if (!OPCODE.compare("ADD")){
-            if (LOC[0] == '='){
+        } else if (!OPCODE.compare("ADD")){ 
+            if (LOC[0] == '='){ // if LOC is a value, add to ACC and store result in ACC
                 ACC += stoi(LOC.substr(1));
-            } else {
+            } else { // if LOC is a variable, add to ACC and store result in ACC
                 ACC += st[LOC];
             }
 
-        } else if (!OPCODE.compare("SUB")){
+        } else if (!OPCODE.compare("SUB")){ //ADD but instead of add to ACC its subtract
             if (LOC[0] == '='){
                 ACC -= stoi(LOC.substr(1));
             } else {
                 ACC -= st[LOC];
             }
 
-        } else if (!OPCODE.compare("MULT")){
+        } else if (!OPCODE.compare("MULT")){ //ADD but multiply
             if (LOC[0] == '='){
                 ACC *= stoi(LOC.substr(1));
             } else {
                 ACC *= st[LOC];
             }
 
-        } else if (!OPCODE.compare("DIV")){
+        } else if (!OPCODE.compare("DIV")){ //ADD but divide
             if (LOC[0] == '='){
                 ACC /= stoi(LOC.substr(1));
             } else {
@@ -90,47 +89,46 @@ int main() {
             }
 
         } else if (!OPCODE.compare("BG")){
-            if (ACC>0){
+            if (ACC>0){ // if ACC is greater than zero, set the pointer to where the BI is in the LABEL field
                 pointer = br[LOC];
-            } else {
+            } else { // just increment the pointer for the next line
                 pointer++;
             }
-        } else if (!OPCODE.compare("BE")){
+        } else if (!OPCODE.compare("BE")){ //BG but only if ACC==0
             if (ACC==0){
                 pointer = br[LOC];
             } else {
                 pointer++;
             }
 
-        } else if (!OPCODE.compare("BL")){
+        } else if (!OPCODE.compare("BL")){ //BG and BE but only if ACC<0
             if (ACC<0){
                 pointer = br[LOC];
             } else {
                 pointer++;
             }
 
-        } else if (!OPCODE.compare("BU")){
+        } else if (!OPCODE.compare("BU")){ //branches to the BI
             pointer = br[LOC];
 
-        } else if (!OPCODE.compare("READ")){
-            cout << "help";
+        } else if (!OPCODE.compare("READ")){ //reads in an integer from the terminal
             cin >> st[LOC];
         }
-        else if (!OPCODE.compare("PRINT")){
+        else if (!OPCODE.compare("PRINT")){ //prints the LOC in the output file
             fout << st[LOC];
         }
-        else if (!OPCODE.compare("DC")){
+        else if (!OPCODE.compare("DC")){ //sets the value of a variable
             st[LABEL] = stoi(LOC);
 
-        } else if (!OPCODE.compare("END")){
+        } else if (!OPCODE.compare("END")){ //ends the code by breaking the while loop
             break;
         }
-        else{
+        else{ //if the OPCODE was not in fact an OPCODE, print error
             cout<<OPCODE<<endl;
             fout << "ERROR";
             break;
         }
-        if (OPCODE[0]!='B'){
+        if (OPCODE[0]!='B'){ // if a branch attempt did not happen, set pointer to the next line
             pointer++;
         }
     }
