45bc643221e4a1a263c42497123bfc662bf057d3
FarmerJohnsBessie <cz52013141834@gmail.com>
2023-06-18 14:52:21 -0700

    Added Trim function, Added Testcases

--- ACSL_Assembly.cpp
+++ ACSL_Assembly.cpp
@@ -2,6 +2,25 @@
 #define ll long long
 using namespace std;
 
+// Trim functions
+static inline std::string &ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+    return ltrim(rtrim(s));
+}
+
 int main() {
     ifstream fin("acslembly.in"); // code file
     ofstream fout("acslembly.out"); // output file(for PRINT OPCODE)
@@ -14,6 +33,7 @@ int main() {
     string line; // Stores the lind read
     while(getline(fin,line)){
 	// Basically a split function
+        line = trim(line);
         stringstream ss(line); 
         string word;
         vector<string> words; // stores the words splited
